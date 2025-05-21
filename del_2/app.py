6aa2fbd18764dcd9e0c96e85f761318d16192676
ShabanSulejmani <Shaabaan_@hotmail.com>
2025-05-21 18:38:49 +0200

    changes

---
+++ del_2/app.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
+import streamlit as st
+import numpy as np
+
+st.title("Diamantanalys för Guldfynd")
+
+
+df = pd.read_csv("diamonds.csv")
+
